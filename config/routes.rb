79cfb46e56f13184acb1a12ba4d3bb24b9368f81
kizaki shinya <skizaki@kizaki-no-MacBook-Air.local>
2018-07-15 15:26:36 +0900

    add routing for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'qustions#index'
   devise_for :users
- get 'questions' => 'questions#index'
- get   'questions/new'  =>  'questions#new'
- post  'questions'      =>  'questions#create'
- get 'athletes'    =>  'athletes#index'
- get   'users/:id'   =>  'users#show'
+  get 'questions' => 'questions#index'
+  get   'questions/new'  =>  'questions#new'
+  post  'questions'      =>  'questions#create'
+  get 'athletes'    =>  'athletes#index'
+  get   'users/:id'   =>  'users#show'
 # get   'athletes/:id' =>  'athletes#show'
 end
