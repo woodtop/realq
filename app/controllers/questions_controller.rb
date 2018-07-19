2be8bd7b417f6ac8a2f8b568912c4fb70678206c
kizaki shinya <woodtop75@aol.com>
2018-07-19 17:04:05 +0900

    トップページに回答ありの質問のみを表示するために、回答ありの質問と回答なしの質問に分類

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,12 @@ class QuestionsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
     def index
-      @questions = Question.order("created_at DESC").page(params[:page]).per(5)
+#      @questions = Question.order("created_at DESC").page(params[:page]).per(5)
+      question_table  = Question.arel_table
+      answer_table = Answer.arel_table
+      condition = answer_table[:question_id].eq(question_table[:id])
+      @questions__has_answer = Question.where(Answer.where(condition).exists).order("created_at DESC").page(params[:page]).per(5)
+      @questions__no_answer  = Question.where(Answer.where(condition).exists.not).order("created_at DESC").page(params[:page]).per(5)
     end
 
     def new
