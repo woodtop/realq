48aec144773e0085edd6110d87c88da9058dd7e6
eichann <k.abe.in401@gmail.com>
2018-07-21 23:25:01 +0900

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715050321) do
+ActiveRecord::Schema.define(version: 20180721134839) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "text",        limit: 65535
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180715050321) do
     t.text     "image",      limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "questions", force: :cascade do |t|
