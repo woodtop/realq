020ccba10595a07be6016ab39ae02c4bc3258a56
eichann <k.abe.in401@gmail.com>
2018-07-21 23:27:20 +0900

    make answers' new page and functions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,19 @@
 class AnswersController < ApplicationController
+
+  before_action :move_to_index, except: [:create]
+
+  def new
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    Answer.create(text: params[:text], question_id: params[:id])
+    @athlete = Athlete.find_by(user_id: current_user.id)
+    render template: "athletes/show"
+  end
+
+
+  def move_to_index
+     redirect_to action: :index unless user_signed_in?
+  end
 end
